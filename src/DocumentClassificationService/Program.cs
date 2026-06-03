0def2c9c962711d603ae0f02d76fe99dcf261121
Leonardo Silva <leofla@gmail.com>
2026-06-03 20:42:10 -0300

    feat: api configurando injecao de dependencia, Polly, resilicencia,  classify endpoint

--- src/DocumentClassificationService/Program.cs
+++ src/DocumentClassificationService/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Polly;
+using Polly.Extensions.Http;
+using DocumentClassificationService.Interfaces;
+using DocumentClassificationService.Services;
+using DocumentClassificationService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<MockDocumentClassifier>();
+
+builder.Services.AddHttpClient<IDocumentClassifier, CerebrasAiClassifier>()
+    .AddTransientHttpErrorPolicy(policyBuilder => 
+        policyBuilder.WaitAndRetryAsync(2, _ => TimeSpan.FromSeconds(1)));
 
 var app = builder.Build();
 
@@ -13,4 +31,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
+app.MapPost("/api/v1/classify", async ([FromBody] DocumentClassificationRequest request, [FromServices] IDocumentClassifier classifier) =>
+{
+    var resultado = await classifier.ClassifyAsync(request);
+    return Results.Ok(resultado);
+});
+
+app.Run();
